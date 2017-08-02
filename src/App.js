577928df5b40cc34591b4b26a24e9ddfe4d193e2
Pharrell.zx <wzxnuaa@gmail.com>
2017-08-02 17:16:42 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import ContactPage from './ContactPage'
+import SimpleForm from './Forms/SimpleForm'
+import SemanticSimpleForm from './Forms/SemanticSimpleForm'
+import { Segment, Divider } from 'semantic-ui-react'
+
+const showResults = values =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      // simulate server latency
+      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+      resolve()
+    }, 100)
+  })
+
+class App extends Component {
+  // noinspection JSMethodCanBeStatic
+  render () {
+    return (
+      <div>
+        <Segment padded>
+          <h2>Getting Started Form</h2>
+          <ContactPage/>
+          <Divider horizontal>Dividing Line</Divider>
+          <h2>Simple Form</h2>
+          <SimpleForm onSubmit={showResults}/>
+          <Divider horizontal>Dividing Line</Divider>
+          <SemanticSimpleForm onSubmit={showResults}/>
+        </Segment>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/ContactPage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ContactForm from './Forms/ContactForm'
+import { store } from './store'
+
+class ContactPage extends React.Component {
+  submit = (values) => {
+    // print the form values to the console
+    console.log(values)
+    console.log(store.state)
+  }
+
+  render() {
+    return (
+      <ContactForm onSubmit={this.submit} />
+    )
+  }
+}
+
+export default ContactPage
